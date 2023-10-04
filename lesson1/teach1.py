2a4f4d181a3748ef41893803709ca7ac63b15716
IDI <ismagulovdimir2005@gmail.com>
2023-10-04 12:37:24 +0300

    Смена файла index.html на index2.html Добавление парсинга имени пользователя

--- lesson1/teach1.py
+++ lesson1/teach1.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 
 
-with open("blank\index2.html", encoding="utf-8") as site:
+with open("blank\\index2.html", encoding="utf-8") as site:
     site_html = site.read()
 # print(site_html)
 
@@ -11,4 +11,10 @@ site_soup = BeautifulSoup(site_html, "lxml")
 # print(title.text)
 
 user_birth_date = site_soup.find('div', class_="user__birth__date")
+user_name = site_soup.find_all(class_='user__name')
+
+print(user_name)
+for e in user_name:
+    print(e.text.strip())
+
 print(user_birth_date.text.strip())
